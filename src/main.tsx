2202b536303d6cadcc37954fe3a7c3a9fdb83345
Evgeny <info.elukas@gmail.com>
2025-02-28 11:37:35 +0300

    install axios and add AppHttpRequests

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { App } from './app/App'
 import { store } from './app/store'
+import { AppHttpRequests } from '@/app/AppHttpRequests.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <AppHttpRequests />
   </Provider>
 )
